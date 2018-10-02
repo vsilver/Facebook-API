c43f603cb13c2abcc34cf87bfc4055d75fceda9e
vsilver <valeriysilvanovich@gmail.com>
2018-10-02 16:34:18 +0300

    Correct get test

--- src/test/java/TestFacebookApi.java
+++ src/test/java/TestFacebookApi.java
@@ -1,18 +1,33 @@
 
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.basic;
 import static org.hamcrest.Matchers.*;
 
 public class TestFacebookApi {
 
-    private static final String URL = "";
+    //private static final String URL = "https://reqres.in";
+
+    protected static void setupRestAssured() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @BeforeTest
+    public void setUp() {
+        setupRestAssured();
+    }
 
     @Test(description = "Create post")
     public void CreatePost(){
         given().
                 contentType(ContentType.JSON).
-         when().
+         when().get("/api/users?page=2")
+                .then().statusCode(200);
     }
+
+    
 }
