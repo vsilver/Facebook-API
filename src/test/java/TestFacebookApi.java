60806c11aeac81ac174face7704fe2179b3a4ee2
vsilver <valeriysilvanovich@gmail.com>
2018-10-03 10:23:48 +0300

    finish test

--- src/test/java/TestFacebookApi.java
+++ src/test/java/TestFacebookApi.java
@@ -4,6 +4,9 @@ import com.jayway.restassured.http.ContentType;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.basic;
 import static org.hamcrest.Matchers.*;
@@ -21,13 +24,50 @@ public class TestFacebookApi {
         setupRestAssured();
     }
 
-    @Test(description = "Create post")
-    public void CreatePost(){
+    @Test(description = "Get user")
+    public void getUser(){
         given().
                 contentType(ContentType.JSON).
          when().get("/api/users?page=2")
                 .then().statusCode(200);
     }
 
-    
+    @Test(description = "Post user")
+    public void postUser(){
+        Map<String,String> user = new HashMap<>();
+        user.put("name", "morpheus" );
+        user.put("job", "leader" );
+
+        given().
+                contentType("application/json")
+                .body(user)
+                .when().post("/api/users")
+                .then().statusCode(201);
+    }
+
+    @Test(description = "Change user")
+    public void changeUser(){
+        Map<String,String> user = new HashMap<>();
+        user.put("name", "morpheus" );
+        user.put("job", "zion resident" );
+
+        given().
+                contentType("application/json")
+                .body(user)
+                .when().put("/api/users")
+                .then().statusCode(200);
+
+    }
+
+    @Test(description = "Delete user")
+    public void deleteUser(){
+        given()
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204);
+
+    }
+
+
 }
